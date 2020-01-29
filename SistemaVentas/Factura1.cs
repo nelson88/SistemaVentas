2a8fb66aac5246645c4ec6c543737b96930dc64f
Nelson Velasquez <NVelasquez@wr.org>
2020-01-29 08:30:00 -0600

    Facturacion

---
+++ SistemaVentas/Factura1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace SistemaVentas
+{
+    public partial class Factura1 : DevExpress.XtraReports.UI.XtraReport
+    {
+        public Factura1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
